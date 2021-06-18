759236ea555e1e3df1875a0a53573bcf3f70dbd7
Vadym Holovin <holyvad@gmail.com>
2021-06-18 18:45:49 +0300

    Start styling home page

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,24 @@ jQuery(function ($) {
   svg4everybody({});
 });
 
+
+
+
+/**
+ * Simplebar
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  const seoScrollable = document.querySelectorAll('.js-seo-content');
+
+  seoScrollable.forEach(el => {
+    const scrollbar = new SimpleBar(el, {
+      autoHide: false,
+      scrollbarMinSize: 50,
+      scrollbarMaxSize: 50,
+    });
+
+    scrollbar.recalculate();
+  });
+});
+
+
